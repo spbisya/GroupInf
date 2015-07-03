022ea8afbea118b3e8fc67d66606460fc9a8ddea
spbisya <spbisya@ya.ru>
2015-07-02 22:20:50 -0400

    Group Inf
    This programm can easily receive basic information about groups from the
    list. List of groups id is on the right side.

---
+++ Group Inf/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Group_Inf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "svobodnoeobshenie",
+            "mskve"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Начать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(278, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(816, 381);
+            this.listBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 62);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "100";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(85, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "93277145";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Сколько";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "С какой\r\nначать?";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(138, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Очистить всё";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 26);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Участников\r\nбольше";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(211, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "93277145";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Проверено";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "0";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(141, 319);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(91, 13);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Tag = "yandex.ru";
+            this.linkLabel1.Text = "★Mdgagj [MC]★";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(138, 372);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(111, 13);
+            this.linkLabel2.TabIndex = 16;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "★Multi-cheats.com★";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(141, 343);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(98, 13);
+            this.linkLabel3.TabIndex = 17;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "★Куратор года★";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(228, 231);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(21, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 397);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Group Parser [by Mdgagj]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Group Inf/Form1.cs
@@ -0,0 +1,179 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_Inf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int group_id = int.Parse(textBox3.Text);
+            for (int i = 0; i < int.Parse(textBox2.Text); i++)
+            {
+                group_id += 1;
+                listBox1.Items.Add(group_id.ToString());
+                listBox1.Update();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string url = @"https://api.vk.com/method/groups.getById?group_id=" + listBox1.Items[i].ToString() + "&fields=screen_name";
+                WebRequest req = WebRequest.Create(url);
+                WebResponse resp = req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                string outer = sr.ReadToEnd();
+                outer = outer.Replace("[", "");
+                outer = outer.Replace("]", "");
+                var json2 = JObject.Parse(outer);
+               if (json2["response"]["is_closed"].ToString() == "0")
+                {
+                    string url1 = @"https://api.vk.com/method/groups.getMembers?group_id=" + listBox1.Items[i].ToString() + "&count=1";
+                    req = WebRequest.Create(url1);
+                     resp = req.GetResponse();
+                    stream = resp.GetResponseStream();
+                     sr = new StreamReader(stream);
+                   outer = sr.ReadToEnd();
+                   // outer = outer.Replace("[", "");
+                  //  outer = outer.Replace("]", "");
+                    var json1 = JObject.Parse(outer);
+                   // textBox4.AppendText(json1.ToString());
+                   // textBox4.Update();
+                    if (int.Parse(json1["response"]["count"].ToString()) > int.Parse(textBox4.Text))
+                    {
+                        listBox2.Items.Add(json2["response"]["name"].ToString() + "     vk.com/" + json2["response"]["screen_name"].ToString());
+                        listBox2.Update();
+                    }
+                   
+                }
+             //  textBox1.AppendText("Ready!" + json2["response"].ToString());    
+               label5.Text = i.ToString();
+               label5.Update();               
+            }
+
+            textBox1.AppendText("Ready!"+"\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            using (StreamWriter sw = new StreamWriter("groups.txt"))
+            {
+                // Первой строкой записываем в файл число строк в нашем списке
+                sw.WriteLine(listBox2.Items.Count.ToString());
+                // В цикле записываем все строки в файл. 
+                // Первая строка в списке имеет индекс 0
+                // Count - число строк в списке
+                for (int j = 0; j < listBox2.Items.Count; j++)
+                    sw.WriteLine(listBox2.Items[j]);
+            }
+            textBox1.AppendText("Saved!" + "\n");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {Random r = new Random();
+            for (int i = 1624; i < 1630; i++)
+            {
+                string token = "e95d74866c70a92feccc654efe6c28c41bdd9a4d3c76970";
+                string time = DateTime.Now.ToString("HH:mm:ss");
+                string url = @"https://api.vk.com/method/messages.send?user_id=240716221&&message="+time+"&attachment=wall70232735_" + i.ToString() + "&access_token=" + token;
+                WebRequest req = WebRequest.Create(url);
+                WebResponse resp = req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                string outer = sr.ReadToEnd();
+               // log.AppendText(outer);
+
+                Thread.Sleep(r.Next(3000, 15000));
+            }
+         //   log.AppendText("Ready!");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://multi-cheats.com/members/mdgagj.1224/");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://multi-cheats.com");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://multi-cheats.com/members/ogonjok.3214/");
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            
+            for (int i = 1; i < 23480;i++ )
+            {
+                try
+                {
+                    string url = "https://api.vk.com/method/groups.getMembers?group_id=70232735&offset=" + i.ToString() + "&fields=can_write_private_message&count=1";
+                    WebRequest req = WebRequest.Create(url);
+                    WebResponse resp = req.GetResponse();
+                    Stream stream = resp.GetResponseStream();
+                    StreamReader sr = new StreamReader(stream);
+                    string outer = sr.ReadToEnd();
+                    outer = outer.Replace("[" + "\n" + "      ", "");
+                    outer = outer.Replace("\n" + "    ]", "");
+                    var json1 = JObject.Parse(outer);
+                    //  if (json1["response"]["users"]["can_write_private_message"].ToString() =="1")
+                    //  {
+                    listBox1.Items.Add(json1["response"]["users"].ToString());
+                    listBox1.Update();
+                    try
+                    {
+                        if (listBox1.Items[i - 1].ToString().Contains("\"can_write_private_message\": 1"))
+                        {
+                            listBox2.Items.Add("vk.com/id" + listBox1.Items[i - 1].ToString().Remove(0, 19).Split(',')[0].ToString());
+                            listBox2.Update();
+                        }
+                    }
+                    catch (Exception l)
+                    {
+                        listBox2.Items.Add(l.Message);
+                        listBox2.Update();
+                    }
+                }
+                catch (Exception k)
+                {
+                    listBox2.Items.Add(k.Message);
+                    listBox2.Update();
+                }
+             //   }
+            }
+            MessageBox.Show("Finished");
+        }
+    }
+}
